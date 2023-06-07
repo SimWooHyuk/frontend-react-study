941d4e6cf59dd4a68d0bb30617bc432566aec362
SimWooHyuk <tladngur2@naver.com>
2023-06-07 15:31:40 +0900

    asdas

--- 10_React/my-todo/src/components/TodoInsert.jsx
+++ 10_React/my-todo/src/components/TodoInsert.jsx
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 import {MdAdd as AddIcon} from 'react-icons/md'
 // Tip: asㄹ르 사용하여 별칭을 붙여 사용하면 추후 아이콘 바꿀때 한곳만 바꾸면 되서 편함
+
+
 const TodoInsertWrapper = styled.form`
   display: flex;
   background: #495057;
@@ -41,10 +43,29 @@ const StyledButton = styled.button`
 
 // 새로운 항목을 입력하고 추가할 수 있는 컴포넌트
 // state를 통해 input의 상태를 관리 (제어컴포넌트)
-function TodoInsert(props) {
+function TodoInsert({onInsert}) {
+
+  const [value, setValue] = useState('');
+
+const handleChange = (e) => {
+  setValue(e.target.value)
+};
+
+const handleSubmit = (e) => {
+  onInsert(value);
+  setValue('') // value값 초기화
+
+  // submit 이벤트가 발생시키는 새로고침을 방지
+  e.preventDefault();
+};
+
+
+
+
+
   return (
-    <TodoInsertWrapper>
-      <StyledInput type='text' placeholder='할 일을 입력하세요.' />
+    <TodoInsertWrapper onSubmit={handleSubmit}>
+      <StyledInput type='text' placeholder='할 일을 입력하세요.' onChange={handleChange} value={value}/>
       <StyledButton type='submit'>
         <AddIcon/>
       </StyledButton>
--- 10_React/my-todo/src/components/TodoListItem.jsx
+++ 10_React/my-todo/src/components/TodoListItem.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
 import { MdCheckBox, MdCheckBoxOutlineBlank, MdRemoveCircleOutline } from 'react-icons/md';
 const TodoListItemWrapper = styled.div`
   padding: 1rem;
@@ -11,11 +11,24 @@ const Checkbox = styled.div`
   cursor: pointer;
   display: flex;
   align-items: center;
+
+  svg{
+    /* 아이콘 스타일링 */
+    font-size: 1.5rem;
+    color: ${props => props.checked && '#22b8cf'};
+  }
 `;
 
 const Text = styled.div`
   margin-left: 0.5rem;
   flex: 1; // 차지할 수 있는 모든 영역 차지
+
+  ${props => props.checked &&
+  css`
+    color: #adb5bd;
+    text-decoration: line-through;
+  `
+  }
 `;
 
 const Remove = styled.div`
@@ -31,20 +44,22 @@ const Remove = styled.div`
 // 각 할 일 항목에 대한 정보를 보여주는 컴포넌트
 // todo 객체를 props로 받아와 상태에 따라 다른 스타일의 UI를 보여줌
 
-function TodoListItem({todo}) {
+function TodoListItem({ todo, onRemove }) {
   // const {todo: {id, text, checked}} = props
 
   // todo로 먼저 한번 분해해서 받은 경우
   const {id, text , checked } = todo;
   return (
     <TodoListItemWrapper>
-      <Checkbox>
-    <MdCheckBoxOutlineBlank />
+      <Checkbox checked={checked}>
+        {checked ? <MdCheckBox /> : <MdCheckBoxOutlineBlank />}
       </Checkbox>
-      <Text>
+      <Text checked={checked}>
         {text}
       </Text>
-      <Remove>
+      <Remove 
+      onClick={() => { onRemove(id);}}
+      >
       <MdRemoveCircleOutline />
       </Remove>
     </TodoListItemWrapper>
