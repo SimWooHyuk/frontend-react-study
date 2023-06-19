ea26bea4a62ea4804c94daa137549690f0ad74fb
SimWooHyuk <tladngur2@naver.com>
2023-06-19 17:08:12 +0900

    asd

--- 10_React/my-news/src/App.js
+++ 10_React/my-news/src/App.js
@@ -1,12 +1,22 @@
 import './App.css';
 import NewsList from './components/NewsList';
+import {Route, Routes} from "react-router-dom"
+import NewsPage from './pages/NewsPage';
 
 function App() {
   return (
-    <>
-    <NewsList />
-    </>
+    // <>
+    // <NewsList />
+    // </>
+
+    <Routes>
+      {/* ?는 CATEGORY 값이 선택적이라는 의미
+      즉, 있을 수도 있고, 없을 수도 있다는 뜻
+       */}
+      <Route path="/:category?" element={<NewsPage />}/>
+    </Routes>
   )
+
 }
 
 export default App;
--- 10_React/my-news/src/components/Categories.jsx
+++ 10_React/my-news/src/components/Categories.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
+import { styled } from 'styled-components';
 
 // 고정값들은 전역 변수로 선언하여 사용
 // 여기서 name은 실제 카테고리 값, text는 렌더링할 때 사용할 한글 카테고리명
@@ -45,11 +47,39 @@ const CategoriesBlock = styled.div`
   }
 `;
 
+// Link 대신 NavLink 사용해보기
+// to 속성에 적은 경로에 따라 active 상태를 알 수 있는 Link 컴포넌트의 특수한 종류
+const Category = styled(NavLink)`
+  font-size: 1.125rem;
+  white-space: pre;
+  text-decoration: none;
+  color: inherit;
+  padding-bottom: 0.25rem;
+  cursor: pointer;
+
+  &:hover {
+    color: #495057;
+  }
+
+  & + & {
+    margin-left: 1rem;
+  }
+`;
+
+
 function Categories(props) {
   return (
-    <div>
-      
-    </div>
+    <CategoriesBlock>
+      {categories.map(c => (
+        <Category
+          key={c.name}
+          to={c.name === 'all' ? '/' : `/${c.name}`}
+
+        >
+
+        </Category>
+      ))}
+    </CategoriesBlock>
   );
 }
 
